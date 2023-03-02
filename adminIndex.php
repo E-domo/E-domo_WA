ee697c876146c14e79d7e5e79bbdb46cf3746cdd
Deldk <114784818+Deldk@users.noreply.github.com>
2023-03-02 20:03:10 +0000

    Access privileges and all

--- adminIndex.php
+++ adminIndex.php
@@ -1,14 +1,11 @@
-	<?php
-	session_start();
+<?php 
+session_start();
 	if (isset($_SESSION['userType']) != 1) {
-			header("Location: logIn.php");
-			exit();
-		}
-	else{
-		echo "success";
-		}
+		header("Location: logIn.php");
+		exit();
+	}
 
-	?>
+ ?>
 
 <!DOCTYPE html>
 <html>
